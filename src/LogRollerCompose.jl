d3bdae8494d55a20141c96d1f3b4ae4e4c3d8fc6
Krystian Guliński <gulinski.krystian@gmail.com>
2023-07-11 13:12:00 +0200

    Update LogRollerCompose.jl

--- src/LogRollerCompose.jl
+++ src/LogRollerCompose.jl
@@ -21,18 +21,6 @@ function logcompose(::Type{LogRoller.RollingLogger}, config::Dict{String,Any}, l
     LogRoller.RollingLogger(filename, sizelimit, nfiles, level; timestamp_identifier=timestamp_identifier, format=format, entry_size_limit=entry_size_limit)
 end
 
-function logcompose(::typeof(LogRoller.RollingFileWriterTee), config::Dict{String,Any}, logger_config::Dict{String,Any})
-    filename = String(strip(logger_config["filename"]))
-    @assert !isempty(filename)
-
-    level = log_assumed_level(logger_config, "Info")
-    sizelimit = get(logger_config, "sizelimit", 10240000)
-    nfiles = get(logger_config, "nfiles", 5)
-    destination_name = logger_config["destination"]
-    destination = LogCompose.logger(config, destination_name)
-    LogRoller.RollingFileWriterTee(filename, sizelimit, nfiles, destination, level)
-end
-
 function logcompose(::typeof(LogRoller.RollingFileWriter), config::Dict{String,Any}, logger_config::Dict{String,Any})
     filename = String(strip(logger_config["filename"]))
     @assert !isempty(filename)
